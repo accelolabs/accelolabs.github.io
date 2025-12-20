f627d2002bb5b7e8f327efee0d3482bf66dbffb0
accelolabs <accelotron@niuitmo.ru>
2025-12-20 10:14:56 +0300

    fix citycard

--- web_labs/src-4/src/components/weather/CityCard.jsx
+++ web_labs/src-4/src/components/weather/CityCard.jsx
@@ -13,7 +13,6 @@ export default function CityCard({ coords, lang, units }) {
       <div className="text-2xl font-bold">{currentWeather.name}</div>
       <div className="text-3xl font-bold">{date.toLocaleTimeString()}</div>
       <div className="text-sm">{date.toDateString()}</div>
-      <div className="text-xl font-semibold">{Math.round(currentWeather.main.temp)}{tempUnit}</div>
     </div>
   );
 }
